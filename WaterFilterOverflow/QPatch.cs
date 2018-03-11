f14850b94e9cba477d527ac15dbe59ea1fb14794
Vlad-00003 <Vlad-00003@mail.ru>
2018-03-11 22:16:33 +0300

    Basicly this mod is nothing but an example of the config usage.

--- WaterFilterOverflow/QPatch.cs
+++ WaterFilterOverflow/QPatch.cs
@@ -1,17 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using Harmony;
+using JetBrains.Annotations;
+using Utilites;
+using Utilites.Config;
 
 namespace WaterFilterOverflow
 {
     public class QPatch
     {
-        private static void Patch()
+        [UsedImplicitly]
+        public static void Patch()
         {
-            HarmonyInstance harmony = HarmonyInstance.Create("com.waterfilteroverflow.mod");
+            Logger.ClearCustomLog(Assembly.GetExecutingAssembly().GetName().Name);
+            FileLog.Reset();
+            Logger.Debug("Starting....");
+            try
+            {
+                //HarmonyInstance harmony = HarmonyInstance.Create("com.waterfilteroverflow.mod");
+                //harmony.PatchAll(Assembly.GetExecutingAssembly());
+                var config =
+                    new ConfigFile(Assembly.GetExecutingAssembly().GetName().Name, "config");
+                config["Option one"]= "Value one";
+                config["Option two"] = 12;
+                config["Third"] = new List<string>() { "cool", "Awsome!" };
+                config["Fourth"] = 0.7f;
+                config["Fith"] = new Dictionary<string,int>()
+                {
+                    ["First key"] = 42
+                };
+                Logger.Info(config.Get<string>("Option one"));
+                Logger.Error(config.Get<string>("Option two"));
+                Logger.Warning(string.Join("\n",config.Get<List<string>>("Third").ToArray()));
+                Logger.Debug(config.Get<float>("Fourth").ToString("P"));
+                var dict = config.Get<Dictionary<string, int>>("Fith");
+                Logger.Log("Logging dictionary from the config");
+                foreach (var i in dict)
+                {
+                    Logger.Log($"Key: {i.Key} | Value {i.Value}");
+                }
+                config.Save();
+            }
+            catch (Exception e)
+            {
+                e.Log(LogType.Harmony);
+            }
         }
     }
 }
