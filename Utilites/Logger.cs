5c336bbad1207eb8a65e23eb1629f175132f4bcc
Vlad-00003 <Vlad-00003@mail.ru>
2018-03-11 14:18:07 +0300

    Utilites moved to the shared project.

---
+++ Utilites/Logger.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Harmony;
+
+namespace Utilites
+{
+    #region Log levels and types.
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Just an information message.
+        /// </summary>
+        Info,
+        /// <summary>
+        /// Something goes wrong, but we can handle it.
+        /// </summary>
+        Warning,
+        /// <summary>
+        /// The error has occur.
+        /// </summary>
+        Error,
+        /// <summary>
+        /// Debug messages that should be removed in the relized versions.
+        /// </summary>
+        Debug
+    }
+
+    public enum LogType
+    {
+        /// <summary>
+        /// Print the information to the file "harmony.log.txt" located at the desktop.
+        /// </summary>
+        Harmony,
+        /// <summary>
+        /// Print the information to the "output_log.txt".
+        /// </summary>
+        Console,
+        /// <summary>
+        /// Print the information on the player screen
+        /// </summary>
+        PlayerScreen
+    }
+    #endregion
+
+    public static class Logger
+    {
+        public static void Debug(string text, LogType type = LogType.Harmony) =>
+            Log(text, LogLevel.Debug, type);
+
+        public static void Error(string text, LogType type = LogType.Harmony) =>
+            Log(text, LogLevel.Error, type);
+
+        public static void Info(string text, LogType type = LogType.Harmony) =>
+            Log(text, LogLevel.Info, type);
+
+        public static void Warning(string text, LogType type = LogType.Harmony) =>
+            Log(text, LogLevel.Warning, type);
+
+        private static void Log(string text, LogLevel level, LogType type)
+        {
+            var caller = Assembly.GetCallingAssembly().GetName().Name;
+            switch (type)
+            {
+                case LogType.Harmony:
+                    Harmony.FileLog.Log($"{DateTime.Now.ToShortTimeString()} [{caller}] [{level}] {text}");
+                    return;
+                case LogType.Console:
+                    Console.WriteLine($"[{caller}] [{level}] {text}]");
+                    return;
+                case LogType.PlayerScreen:
+                    ErrorMessage.AddDebug($"{DateTime.Now.ToShortTimeString()} [{caller}] [{level}] {text}");
+                    return;
+            }
+        }
+
+        #region Exceptions
+        public static void Log(this Exception e) =>
+            Debug(FormatException(e));
+        private static string FormatException(Exception e)
+        {
+            if (e == null)
+                return string.Empty;
+            return $"\"Exception: {e.GetType()}\"\n\tMessage: {e.Message}\n\tStacktrace: {e.StackTrace}\n" +
+                   FormatException(e.InnerException);
+        }
+        #endregion
+
+        #region Harmony
+
+        public static void Log(this IEnumerable<CodeInstruction> instructions)
+        {
+            Debug($"Logging instuctions");
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                Debug($"[{i}] {codes[i]}");
+            }
+            Debug($"End of instuctions log");
+        }
+
+        public static void LogPatches(this MethodBase method, HarmonyInstance harmony)
+        {
+            var patches = harmony.IsPatched(method);
+            if (patches == null)
+            {
+                Debug($"Method \"{method}\" is not patched!");
+                return;
+            }
+            Debug("Logging Prefixes...");
+            foreach (var patch in patches.Prefixes)
+            {
+                Debug($"Patch {patch.index}:\n\tOwner: {patch.owner}\n\tPatched method: {patch.patch}\n\tPriority: {patch.priority}\n\tBefore: {patch.before}\n\tAfter:{patch.after}");
+            }
+            Debug("Logging Postfixes...");
+            foreach (var patch in patches.Postfixes)
+            {
+                Debug($"Patch {patch.index}:\n\tOwner: {patch.owner}\n\tPatched method: {patch.patch}\n\tPriority: {patch.priority}\n\tBefore: {patch.before}\n\tAfter:{patch.after}");
+            }
+            Debug("Loggind Transpilers...");
+            foreach (var patch in patches.Transpilers)
+            {
+                Debug($"Patch {patch.index}:\n\tOwner: {patch.owner}\n\tPatched method: {patch.patch}\n\tPriority: {patch.priority}\n\tBefore: {patch.before}\n\tAfter:{patch.after}");
+            }
+            Debug("Done!");
+        }
+
+        #endregion
+    }
+}
---
+++ Utilites/ReflectionUtilites.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Utilites;
+namespace Utilites
+{
+    internal static class ReflectionUtilites
+    {
+
+        private const BindingFlags Flags =
+            BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.Static |
+            BindingFlags.Instance |
+            BindingFlags.DeclaredOnly;
+
+        #region Fields
+
+        public static IEnumerable<FieldInfo> GetAllFields(this Type type) =>
+            type?.GetFields(Flags).Union(GetAllFields(type.BaseType)) ?? Enumerable.Empty<FieldInfo>();
+
+        public static void LogAllFields(this Type type)
+        {
+            Logger.Debug("Logging all Fields");
+            foreach (var info in type.GetAllFields())
+                Log(info);
+            Logger.Debug("End of Fields");
+        }
+
+        public static void Log(this FieldInfo info) =>
+            Logger.Debug($"Field: \"{info}\"\n{info?.Attributes}");
+
+        public static void Log(this FieldInfo info, object obj) =>
+            Logger.Debug($"Field: \"{info}\". Value: \"{info?.GetValue(obj)}\"\n{info?.Attributes}");
+        #endregion
+
+        #region Methods
+
+        public static IEnumerable<MethodInfo> GetAllMethods(this Type type) =>
+            type?.GetMethods(Flags).Union(GetAllMethods(type.BaseType)) ?? Enumerable.Empty<MethodInfo>();
+
+        public static void LogAllMethods(this Type type)
+        {
+            Logger.Debug("Logging all Methods");
+            foreach (var info in type.GetAllMethods())
+                Log(info);
+            Logger.Debug("End of Methods");
+        }
+
+        public static void Log(this MethodInfo info) =>
+            Logger.Debug($"Method: \"{info}\"\n\"{info?.Attributes}\"");
+
+        #endregion
+
+        #region Constructors
+
+        public static IEnumerable<ConstructorInfo> GetAllConstructors(this Type type) =>
+            type?.GetConstructors(Flags) ?? Enumerable.Empty<ConstructorInfo>();
+
+        public static void LogAllConstructors(this Type type)
+        {
+            Logger.Debug("Logging all Constructors");
+            foreach (var info in type.GetAllConstructors())
+                Log(info);
+            Logger.Debug("End of Constructors");
+        }
+
+        public static void Log(this ConstructorInfo info)=>
+            Logger.Debug($"Constructor: {info}\n{info?.Attributes}");
+        #endregion
+    }
+}
