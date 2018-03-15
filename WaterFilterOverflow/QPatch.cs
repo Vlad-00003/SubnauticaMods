02a01037d20f7eeac013c13aec972b309fc3b036
Vlad-00003 <Vlad-00003@mail.ru>
2018-03-15 14:41:10 +0300

    Some wired modifications. Does absolutly nothin.

--- WaterFilterOverflow/QPatch.cs
+++ WaterFilterOverflow/QPatch.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reflection;
 using Harmony;
 using JetBrains.Annotations;
-using Utilites;
 using Utilites.Config;
+using Utilites.Logger;
 
 namespace WaterFilterOverflow
 {
@@ -14,37 +13,20 @@ namespace WaterFilterOverflow
         public static void Patch()
         {
             Logger.ClearCustomLog(Assembly.GetExecutingAssembly().GetName().Name);
-            FileLog.Reset();
-            Logger.Debug("Starting....");
             try
             {
                 //HarmonyInstance harmony = HarmonyInstance.Create("com.waterfilteroverflow.mod");
                 //harmony.PatchAll(Assembly.GetExecutingAssembly());
-                var config =
-                    new ConfigFile(Assembly.GetExecutingAssembly().GetName().Name, "config");
-                config["Option one"]= "Value one";
-                config["Option two"] = 12;
-                config["Third"] = new List<string>() { "cool", "Awsome!" };
-                config["Fourth"] = 0.7f;
-                config["Fith"] = new Dictionary<string,int>()
-                {
-                    ["First key"] = 42
-                };
-                Logger.Info(config.Get<string>("Option one"));
-                Logger.Error(config.Get<string>("Option two"));
-                Logger.Warning(string.Join("\n",config.Get<List<string>>("Third").ToArray()));
-                Logger.Debug(config.Get<float>("Fourth").ToString("P"));
-                var dict = config.Get<Dictionary<string, int>>("Fith");
-                Logger.Log("Logging dictionary from the config");
-                foreach (var i in dict)
-                {
-                    Logger.Log($"Key: {i.Key} | Value {i.Value}");
-                }
+                var config = new ConfigFile(Assembly.GetExecutingAssembly().GetName().Name, "config");
+                config.Load();
+                string first = "Fifffff";
+                config.TryGet(ref first, "Step 1", "Step 2", "Step 3", "First option");
+                Logger.Log(first);
                 config.Save();
             }
             catch (Exception e)
             {
-                e.Log(LogType.Harmony);
+                e.Log();
             }
         }
     }
