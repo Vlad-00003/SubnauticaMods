b394077dbe373c2bae10f69aca18b24fc53ef6b4
Vlad-00003 <Vlad-00003@mail.ru>
2018-03-15 20:26:29 +0300

    New mod added =)

---
+++ LargeDepositsFix/Drillable_DestroySelf_Patch.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace LargeDepositsFix
+{
+    [HarmonyPatch(typeof(Drillable), "DestroySelf")]
+    class Drillable_DestroySelf_Patch
+    {
+        static bool Prefix(Drillable __instance)
+        {
+            __instance.SendMessage("OnBreakResource",null, SendMessageOptions.DontRequireReceiver);
+            return true;
+        }
+    }
+}
---
+++ LargeDepositsFix/QPatch.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+
+namespace LargeDepositsFix
+{
+    public class QPatch
+    {
+        public static void Patch()
+        {
+            try
+            {
+                HarmonyInstance harmony = HarmonyInstance.Create("com.drillablescan.mod");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"DrillableScan patch failed!\n{FormatException(e)}");
+            }
+        }
+        private static string FormatException(Exception e)
+        {
+            if (e == null)
+                return string.Empty;
+            return $"\"Exception: {e.GetType()}\"\n\tMessage: {e.Message}\n\tStacktrace: {e.StackTrace}\n" +
+                   FormatException(e.InnerException);
+        }
+    }
+}
