18294fe0815bca0dac174e4de7a6566cf521119c
Vlad-00003 <Vlad-00003@mail.ru>
2018-03-20 16:08:52 +0300

    QuantumStoarge. Currently WIP.

---
+++ QuantumStorage/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Storage that allow you to store large amount of the items in one place. But a single storage can hold only 1 type of the items.")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vlad-00003")]
+[assembly: AssemblyProduct("QuantumStorage")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2e7b69ae-657e-4269-b0e6-95f1eb40d1ff")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
---
+++ QuantumStorage/QPatch.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using SMLHelper;
+using SMLHelper.Patchers;
+using UnityEngine;
+using Logger = Utilites.Logger.Logger;
+using Object = UnityEngine.Object;
+
+namespace QuantumStorage
+{
+    public class QPatch
+    {
+        public static void Patch()
+        {
+            Logger.ClearCustomLog();
+            Logger.Debug("Loading start...");
+
+            var ab = AssetBundle.LoadFromFile(
+                $"./QMods/{Assembly.GetExecutingAssembly().GetName().Name}/testing.assets");
+            Logger.Debug("Bundle name: " + ab.name);
+
+            var cube = ab.LoadAsset<GameObject>("Cube");
+            Logger.Debug("Cube name: " + cube.name);
+
+            Utility.AddBasicComponents(ref cube, "Cube");
+            Logger.Debug("BasicComponents added!");
+
+            var techType = TechTypePatcher.AddTechType("Cube", "Cube", "Just a funny cube");
+            Logger.Debug("TechType created!");
+
+            var constructable = cube.AddComponent<Constructable>();
+            constructable.allowedInBase = true;
+            constructable.allowedOnGround = true;
+            constructable.allowedInSub = true;
+            constructable.allowedOnCeiling = false;
+            constructable.allowedOnConstructables = true;
+            constructable.allowedOnWall = true;
+            constructable.allowedOutside = true;
+            constructable.techType = techType;
+            var model = cube.FindChild("Sphere");
+            constructable.model = model;
+            var modelname = model?.name ?? "NULL";
+            Logger.Debug($"Constructable added! Model: {modelname}");
+
+            var techTag = cube.AddComponent<TechTag>();
+            techTag.type = techType;
+            Logger.Debug("techTag added!");
+
+            var rb = cube.GetComponent<Rigidbody>();
+            Object.DestroyImmediate(rb);
+            Logger.Debug("Rigitbody killed!");
+
+            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab("Cube", "Submarine/Build/Cube",cube,techType));
+            Logger.Debug("Customprefab added!");
+
+            var techData = new TechDataHelper
+            {
+                _ingredients = new List<IngredientHelper>()
+                {
+                    new IngredientHelper(TechType.Titanium, 1)
+                },
+                _techType = techType
+            };
+            Logger.Debug("techData added!");
+
+            CraftDataPatcher.customTechData.Add(techType,techData);
+            Logger.Debug("techData added to custom list!");
+
+            CraftDataPatcher.customBuildables.Add(techType);
+            Logger.Debug("techType added to custom Buildables!");
+
+            var groups = typeof(CraftData).GetField("groups", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                .GetValue(null) as Dictionary<TechGroup, Dictionary<TechCategory, List<TechType>>>;
+            groups[TechGroup.InteriorModules][TechCategory.InteriorModule].Add(techType);
+            Logger.Debug("techType added to the groups!");
+        }
+    }
+}
