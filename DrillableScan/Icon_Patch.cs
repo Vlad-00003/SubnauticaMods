a6a68ce0139dcbd1cbc2d8f415ebaece7d398eca
nary_believe <nary.believe@gmail.com>
2023-12-03 11:15:38 -0800

    Added icons for "drillable" items in the "Scanning..." GUI

--- DrillableScan/Icon_Patch.cs
+++ DrillableScan/Icon_Patch.cs
@@ -79,4 +79,81 @@ namespace DrillableScan
             }
         }
     }
+    
+    [HarmonyPatch(typeof(uGUI_MapRoomScanner), "UpdateGUIState")]
+    class uGUI_MapRoomScanner_UpdateGUIState_Patch
+    {
+        static void Postfix(uGUI_MapRoomScanner __instance) 
+        {
+          switch (__instance.mapRoom.GetActiveTechType())
+          {
+              case TechType.DrillableSalt:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Salt);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableQuartz:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Quartz);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableCopper:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Copper);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableTitanium:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Titanium);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableLead:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Lead);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableSilver:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Silver);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableDiamond:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Diamond);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableGold:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Gold);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableMagnetite:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Magnetite);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableLithium:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Lithium);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableMercury:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.MercuryOre);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableUranium: 
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Uranium);
+                __instance.scanningIcon.enabled = true;
+                break; 
+              case TechType.DrillableNickel:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Nickel);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableSulphur:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Sulphur);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableKyanite:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.Kyanite);
+                __instance.scanningIcon.enabled = true;
+                break;
+              case TechType.DrillableAluminiumOxide:
+                __instance.scanningIcon.sprite = SpriteManager.GetWithNoDefault(TechType.AluminumOxide);
+                __instance.scanningIcon.enabled = true;
+                break;
+              default:
+                break;
+          }
+        }
+    }
 }
